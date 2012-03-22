2c58ff0def5e470f7b0aa3acde02b2f453da4d4c
Ralf Mueller <ralf.mueller@zmaw.de>
2012-03-22 10:07:20 +0100

    Release 1.0.7 of cdo.{rb,py}

--- python/setup.py
+++ python/setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 setup (name   = 'cdo',
-  version     = '1.0.7rc',
+  version     = '1.0.7',
   author      = "Ralf Mueller",
   author_email= "stark.dreamdetective@gmail.com",
   license     = "GPLv2",
