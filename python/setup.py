8e83fc23ac285227311abf6dbe747ab3d9ac9b12
Ralf Mueller <ralf.mueller@zmaw.de>
2016-10-25 12:25:27 +0200

    new relase 1.3.2 [ruby/python]
    * improved handling of stderr and stdout (thx @jvegasbsc)
    * improved handling of operators which do _not_ create output files, but write to stdout (thx @illing2005)

--- python/setup.py
+++ python/setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 setup (name   = 'cdo',
-  version     = '1.3.0',
+  version     = '1.3.2',
   author      = "Ralf Mueller",
   author_email= "stark.dreamdetective@gmail.com",
   license     = "GPLv2",
