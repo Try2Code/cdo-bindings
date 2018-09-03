93e0f27fb248c6a529ccb43ca71ebfdb959f2702
Ralf Mueller <ralf.mueller@mpimet.mpg.de>
2018-09-03 11:40:13 +0200

    increate release to 1.3.7

--- python/test/test_cdo.py
+++ python/test/test_cdo.py
@@ -51,7 +51,7 @@ class CdoTest(unittest.TestCase):
 
     def testVersions(self):
         cdo = Cdo()
-        self.assertEqual('1.3.6',cdo.__version__())
+        self.assertEqual('1.3.7',cdo.__version__())
         self.assertTrue(parse_version('1.7.0') <= parse_version(cdo.version()))
 
     def testCDO(self):
