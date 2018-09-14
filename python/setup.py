cd0b8a72dd4a3aadb55540f9d472e8425ffbc2dd
Ralf Mueller <ralf.mueller@mpimet.mpg.de>
2018-09-14 13:17:19 +0200

    increase version to 1.3.8 - this version
    will NOT be released. its just a placeholder for collecting all
    features for 1.4.0

--- python/setup.py
+++ python/setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 setup (name   = 'cdo',
-  version     = '1.3.7',
+  version     = '1.3.8',
   author      = "Ralf Mueller",
   author_email= "stark.dreamdetective@gmail.com",
   license     = "GPLv2",
