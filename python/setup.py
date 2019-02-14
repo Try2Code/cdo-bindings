7b8adfd71819b0e04035525704f2e91e3a7ff1d1
Ralf Mueller <ralf.mueller@dkrz.de>
2019-02-14 11:43:29 +0100

    [py] fix license handling in setup.py

--- python/setup.py
+++ python/setup.py
@@ -114,7 +114,7 @@ setup(
     classifiers=[
         # Trove classifiers
         # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
-        'License :: OSI Approved :: MIT License',
+        license,
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
